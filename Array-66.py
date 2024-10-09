9363cbed13cf5a1fc088059e13c08672177e0b73
abdulrehman36 <123913847+abdulrehman36@users.noreply.github.com>
2024-10-09 16:15:05 -0600

    finished question 66

--- Array-66.py
+++ Array-66.py
@@ -4,22 +4,17 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
-
         for x in range(len(digits) - 1, -1, -1):
-            if digits[x] == 9: # if our last digit is 9
-                if len(digits) >= 2:
-                    digits[x] = 0 # change last digit to 0
-                    digits[x - 1] += 1 # add 1 to the prev index
-                else:
-                    digits[x] = 1
-                    digits.append(0)
-                return digits
-
-            elif digits[x] < 9:
+            if digits[x] == 9:
+                digits[x] = 0
+            else:
                 digits[x] += 1
                 return digits
 
+        digits.insert(0, 1)
+        return digits
+
 
 solution_instance = Solution()
-result = solution_instance.plusOne([9,9]) # 1 2 3 0
-print(result)
+result = solution_instance.plusOne([1,2,3,4])
+print(result)  # Output: [1, 0, 0]
