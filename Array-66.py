e7a37bc55f5c8e1045e074ee739b312e6d7e5952
abdulrehman36 <123913847+abdulrehman36@users.noreply.github.com>
2024-10-08 23:11:10 -0600

    Working on 66, small bug. Failed on test case (9,9). Output was (10,0). Should have been [1,0,0]

--- Array-66.py
+++ Array-66.py
@@ -6,15 +6,20 @@ class Solution(object):
         """
 
         for x in range(len(digits) - 1, -1, -1):
-            if digits[x] >= 9:
-                digits[x] = 0
-                digits[x - 1] += 1
+            if digits[x] == 9: # if our last digit is 9
+                if len(digits) >= 2:
+                    digits[x] = 0 # change last digit to 0
+                    digits[x - 1] += 1 # add 1 to the prev index
+                else:
+                    digits[x] = 1
+                    digits.append(0)
                 return digits
+
             elif digits[x] < 9:
                 digits[x] += 1
                 return digits
 
 
 solution_instance = Solution()
-result = solution_instance.plusOne([9]) # 1 2 3 0
+result = solution_instance.plusOne([9,9]) # 1 2 3 0
 print(result)
