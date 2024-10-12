b68666a343c71dd2ba23c7a6f8786fecfea4741f
abdulrehman36 <123913847+abdulrehman36@users.noreply.github.com>
2024-10-11 22:04:44 -0600

    Im pretty much done but code doesnt run fast. Need to implement it faster than O(n^2)

--- Array-217.py
+++ Array-217.py
@@ -1,1 +1,20 @@
-gi
+#Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        for i in range(len(nums) - 1):
+            for x in range(i+1,len(nums)):
+                if nums[i] == nums[x]:
+                    return True
+
+        return False
+
+
+solution_instance = Solution()
+result = solution_instance.containsDuplicate([1,2,3,4,5])
+
+print(result)
