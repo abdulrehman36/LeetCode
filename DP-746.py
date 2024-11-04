ad3ac6e222f01b6c221fee7b7ee3f1f1f7641639
abdulrehman36 <123913847+abdulrehman36@users.noreply.github.com>
2024-11-04 17:02:54 -0600

    finished DP 746, done pretty easily

--- DP-746.py
+++ DP-746.py
@@ -4,12 +4,36 @@ class Solution(object):
         :type cost: List[int]
         :rtype: int
         """
+        #   Base Case
+        if len(cost) == 1:
+            return cost[0]
+
+        n = len(cost)
+        dp = [0] * n
+        dp[-1] = cost[-1] # initializing table
+        dp[-2] = cost[-2]
+
+        for step in range(n-3,-1,-1):
+            dp[step] = cost[step] + min(dp[step+1],dp[step+2]) # main step
+
+        return min(dp[0],dp[1])
+
+
+solution = Solution()
+print(solution.minCostClimbingStairs([1,100,1,1,1,100,1,1,100,1]))
 
 
 
 
 
 
+
+#
+#
+#   minCostClimbingStairs(cost) =  {
+#                                   cost[0] <- if cost.length = 1
+#                                   dp[i] = cost[i] + min(dp[i+1],dp[i+2]) <- otherwise
+#
 
 
 
