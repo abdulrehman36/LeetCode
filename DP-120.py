23525641e1f71923d624b61509e96985bd3b5671
abdulrehman36 <123913847+abdulrehman36@users.noreply.github.com>
2024-11-04 11:22:49 -0600

    had to get the solution from online. Not my personal work

--- DP-120.py
+++ DP-120.py
@@ -1,21 +1,21 @@
-class Solution(object):
+class Solution:
     def minimumTotal(self, triangle):
-        """
-        :type triangle: List[List[int]]
-        :rtype: int
-        """
+        # Initialize a dp array with the values of the last row in the triangle
+        dp = triangle[-1]
 
-        dp = []
+        # Iterate from the second-last row up to the first row
+        for row in range(len(triangle) - 2, -1, -1):
+            # Update dp array by storing minimum path sum for each position
+            for col in range(len(triangle[row])):
+                dp[col] = triangle[row][col] + min(dp[col], dp[col + 1])
 
-        for row in triangle:
-            min_value = min(row)
-            dp.append(min_value)
-
-        return sum(dp)
+        # The minimum path sum starting from the top will be in dp[0]
+        return dp[0]
 
 
+# Example usage
 solution = Solution()
-print(solution.minimumTotal([[-1],[2,3],[1,-1,-3]]))
+print(solution.minimumTotal([[-1], [2, 3], [1, -1, -3]]))
 
 ##
 #   -1
