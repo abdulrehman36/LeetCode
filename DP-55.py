633c99a6e0ac677e27dae65b2385a49391c116b9
abdulrehman36 <123913847+abdulrehman36@users.noreply.github.com>
2024-11-04 20:03:49 -0600

    will resume later

--- DP-55.py
+++ DP-55.py
@@ -4,3 +4,22 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
+
+        if len(nums) == 1:
+            return True
+
+        n = len(nums)
+        # initializing the table
+        dp = [True for i in range(n)]
+
+        for i in range(n-2,-1,-1):
+            if nums[i] <= nums[i + 1]:  # take jump
+                dp[i] = True
+
+
+
+        print(dp)
+
+
+solution = Solution()
+print(solution.canJump([1,0,3]))
