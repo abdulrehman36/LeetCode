3429a85416b0746a2cc6da5401970914ed86e0cf
abdulrehman36 <123913847+abdulrehman36@users.noreply.github.com>
2024-12-05 12:05:33 -0600

    Starting to work on greedy algorithms, starting 455 Assign Cookies

---
+++ D-455.py
@@ -0,0 +1,9 @@
+# 455. Assign Cookies
+
+class Solution(object):
+    def findContentChildren(self, g, s):
+        """
+        :type g: List[int]
+        :type s: List[int]
+        :rtype: int
+        """
