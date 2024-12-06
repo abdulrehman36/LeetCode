2be9544ebdc4d55b7ca2e211ca0ebf3b5daed306
abdulrehman36 <123913847+abdulrehman36@users.noreply.github.com>
2024-12-06 13:27:28 -0600

    added some comments

--- DP-392.py
+++ DP-392.py
@@ -25,3 +25,29 @@ class Solution(object):
 
 solution = Solution()
 print(solution.isSubsequence(["abc"],["ace"]))
+
+# Notes
+# n = len(s)
+#         dp = [0] * len(t)
+#         #dp[0] = s[0] # initializing table
+#
+#
+#         if len(s) > len(t): # base case
+#             return False
+#         j = 0
+#         for i in range(len(s[0])):
+#             if s[0][i] == t[0][j]:
+#                 dp[i] = t[0][i]
+#             else:
+#                 j += 1
+#
+#             if j == len(t[0]):
+#                 return False
+
+# i tried a dp solution but its easier to use pointers
+# my entire idea of keeping track of indices is flawed.
+
+# solution is as follows
+# if the two letters match, increment i and j.
+# if they dont match, increment j
+# at the end, check if i == len(s) return true.
