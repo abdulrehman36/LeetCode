766708fdd728f5565f6c935600df15d6f503d420
abdulrehman36 <123913847+abdulrehman36@users.noreply.github.com>
2024-12-09 13:53:11 -0600

    finished DFS-230

--- DFS-230.py
+++ DFS-230.py
@@ -1,9 +1,9 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution(object):
     def kthSmallest(self, root, k):
         """
@@ -11,3 +11,28 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+
+        def inorder(node):
+            if not node:
+                return
+            inorder(node.left)
+            visit[0] += 1
+            if visit[0] == k:
+                result[0] = node.val
+                return
+            inorder(node.right)
+
+        visit = [0]
+        result = [None]
+        inorder(root)
+        return result[0]
+
+
+# My algorithm is as follows
+# do an inorder traversal of the BST,
+# Because of the BST property and because of the inorder traversal
+# the first element we visit is the smallest element in the tree
+# since we are trying to find the kth smallest, everytime we visit a node increment visit
+# when visit == k, return that node as it is the kth smallest element
+
+# Runtime: O(n)
